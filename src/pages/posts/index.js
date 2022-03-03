f7cf253a5e73039c8c5d03170e5e4360b22ee7d5
Kelvin Welter <kelvinwelter.b@gmail.com>
2022-03-03 01:18:42 -0300

    create blogposts page

---
+++ src/pages/posts/index.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { graphql } from 'gatsby';
+import Navbar from '../../components/Navbar';
+import BlogpostCard from '../../components/BlogpostCard';
+import { Center, Flex, Heading } from '@chakra-ui/react';
+
+const PostsPage = ({ data }) => {
+  return (
+    <Flex as="main" direction="column">
+      <Navbar/>
+      <Center maxW="48rem" flexGrow={1} flexDirection="column" alignSelf="center">
+        <Heading>Posts</Heading>
+        {
+          data.allMdx.nodes.map((node) => (
+            <BlogpostCard node={node} />
+          ))  
+        }
+      </Center>
+    </Flex>
+  )
+}
+
+export const query = graphql`
+    query getPosts {
+        allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+            nodes {
+                frontmatter {
+                    date(formatString: "MMMM D, YYYY")
+                    title
+                    preview
+                }
+                id
+                slug
+            }
+        }
+    }
+`;
+
+export default PostsPage;
