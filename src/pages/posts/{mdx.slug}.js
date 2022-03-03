832675619a0c6028d950dba91e0417c9a7b46940
Kelvin Welter <kelvinwelter.b@gmail.com>
2022-03-03 01:19:16 -0300

    create blog post page

---
+++ src/pages/posts/{mdx.slug}.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { graphql } from 'gatsby'
+import Navbar from '../../components/Navbar';
+import MDXStyleProvider from '../../components/MDXStyleProvider';
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { Box, Flex, Heading, Text } from '@chakra-ui/react';
+
+const BlogPost = ({ data }) => {
+    return (
+        <Flex as="main" direction="column">
+          <Navbar/>
+          <Box p={16} maxW="48rem" flexGrow={1} flexDirection="column" alignSelf="center">
+            <Heading as="h1" color="gray.700" textAlign="center">{data.mdx.frontmatter.title}</Heading>
+            <Text pb={4} color="gray.500" fontSize="sm" textAlign="center">{data.mdx.frontmatter.date}</Text>
+            <MDXStyleProvider>
+              <MDXRenderer>
+                {data.mdx.body}
+              </MDXRenderer>
+            </MDXStyleProvider>
+          </Box>
+        </Flex>
+      )
+}
+
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: {eq: $id}) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+        preview
+      }
+      body
+    }
+  }
+`;
+
+export default BlogPost
