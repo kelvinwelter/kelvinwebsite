5828dcc90efb6c4991263c24a7813b991dcdbbe6
Kelvin Welter <kelvinwelter.b@gmail.com>
2022-03-04 20:39:27 -0300

    add projects to projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,17 +1,38 @@
 import * as React from 'react';
+import { graphql } from 'gatsby';
 import Navbar from '../components/Navbar';
+import ProjectCard from '../components/ProjectCard';
 import { Center, Flex, Heading, Text } from '@chakra-ui/react';
 
-const ProjectsPage = () => {
+const ProjectsPage = ({ data }) => {
   return (
     <Flex as="main" direction="column">
       <Navbar/>
-      <Center maxW="48rem" flexGrow={1} flexDirection="column" alignSelf="center">
-        <Heading mb={8}>Projects</Heading>
-        <Text>Coming soon 👀</Text>
+      <Center maxW="48rem" flexGrow={1} flexDirection="column" alignSelf="center" paddingX={4}>
+        <Heading>Projects</Heading>
+        {
+          data.allMdx.nodes.map((node) => (
+            <ProjectCard node={node} />
+          ))  
+        }
       </Center>
     </Flex>
   )
 }
+
+export const query = graphql`
+    query {
+        allMdx(sort: {fields: frontmatter___date, order: DESC}, filter: {fileAbsolutePath: {regex: "/content/projects/"}}) {
+            nodes {
+                frontmatter {
+                    title
+                    preview
+                    link
+                }
+                id
+            }
+        }
+    }
+`;
 
 export default ProjectsPage;
